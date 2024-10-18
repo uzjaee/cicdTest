9479396343c710878c4b416cd76da0de6746f613
uzjaee <kyj960802@gmail.com>
2024-10-18 16:26:45 +0900

    feat : cicd msa 환경에서 테스트를 위한 파일 구조 변경

---
+++ user/src/main/java/com/cicd/user/UserController.java
@@ -0,0 +1,13 @@
+package com.cicd.user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @GetMapping("/users/{userId}")
+    public String getUser(@PathVariable("userId") String userId) {
+        return "Hello" + userId + "user!";
+    }
+}
---
+++ user/src/test/java/com/cicd/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicd.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
